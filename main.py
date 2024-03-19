efaf784495d67e036df4d2b1102d10aa80375a11
Mike20011986 <mishgun20011986@gmail.com>
2024-03-19 23:20:07 +0300

    get_params_and_post_data

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-from json.decoder import JSONDecodeError
 import requests
 
 
-response = requests.get("https://playground.learnqa.ru/api/get_text")
+response = requests.post("https://playground.learnqa.ru/api/check_type", data={"param1":"value1"})
 print(response.text)
-
-try:
-    parsed_response_text = response.json()
-    print(parsed_response_text["answer"])
-except JSONDecodeError:
-    print("Response is not a JSON format")

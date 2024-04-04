6967c7ea486ee53dce056290b3e310984974480b
Mike20011986 <mishgun20011986@gmail.com>
2024-04-04 12:11:22 +0300

    After 2 video in lesson 4

--- lib/assertions.py
+++ lib/assertions.py
@@ -12,3 +12,15 @@ class Assertions:
 
         assert name in response_as_dict, f"Response JSON doesn't have key {name}"
         assert response_as_dict[name] == expected_value, error_message
+
+    def assert_json_has_key(response: Response, name):
+        try:
+            response_as_dict = response.json()
+        except json.JSONDecodeError:
+            assert False, f"Response is not JSON format. Response text is '{response.text}'"
+
+        assert name in response_as_dict, f"Response JSON doesn't have key {name}"
+
+    def assert_code_status(response: Response, expected_status_code):
+        assert response.status_code == expected_status_code, \
+            f"Unexpected status code! Expected: {expected_status_code}. Actual: {response.status_code}"
--- tests/test_user_register.py
+++ tests/test_user_register.py
@@ -1,9 +1,30 @@
 import requests
 from lib.base_case import BaseCase
 from lib.assertions import Assertions
+from datetime import datetime
 
 
 class TestUserRegister(BaseCase):
+    def setup(self):
+        basepart = "learnqa"
+        domain = "example.com"
+        randompart = datetime.now().strftime("%m%d%y%H%M%S")
+        self.email = f"{basepart}{randompart}@{domain}"
+
+    def test_create_user_successfully(self):
+        data = {
+            'password': '123',
+            'username': 'learnqa',
+            'firstName': 'learnqa',
+            'lastName': 'learnqa',
+            'email': self.email
+        }
+
+        response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
+
+        Assertions.assert_code_status(response, 200)
+        Assertions.assert_json_has_key(response, "id")
+
     def test_create_user_with_existing_email(self):
         email = 'vinkotov@example.com'
         data = {
@@ -16,5 +37,6 @@ class TestUserRegister(BaseCase):
 
         response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
 
-        assert response.status_code == 400, f"Unexpected status code {response.status_code}"
-        assert response.content.decode('utf-8') == f"Users with email '{email}' already exists", f"Unexpected response content {response.content}"
+        Assertions.assert_code_status(response, 400)
+        assert response.content.decode('utf-8') == f"Users with email '{email}' already exists", \
+            f"Unexpected response content {response.content}"

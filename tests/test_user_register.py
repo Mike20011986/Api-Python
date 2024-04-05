48c711f6ac500b0ade7ef1ecbe78576812588b1d
Mike20011986 <mishgun20011986@gmail.com>
2024-04-05 11:15:40 +0300

    After 3 video in 4 lesson

--- tests/test_user_register.py
+++ tests/test_user_register.py
@@ -40,3 +40,7 @@ class TestUserRegister(BaseCase):
         Assertions.assert_code_status(response, 400)
         assert response.content.decode('utf-8') == f"Users with email '{email}' already exists", \
             f"Unexpected response content {response.content}"
+
+    
+
+
